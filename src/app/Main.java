bc2a8718628b342e2f244477acab0d7f31552e42
Yihang Cai <Yihang.cai@mail.utoronto.ca>
2023-10-23 15:00:51 -0400

    example ui created

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,7 @@
 package app;
 import javax.swing.*;
+import java.awt.*;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -12,10 +14,16 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 200);
 
+        frame.getContentPane().setBackground(Color.GRAY);
+
         // Create a button and add it to the frame
         JButton button = new JButton("Click Me!");
         button.addActionListener(e -> JOptionPane.showMessageDialog(frame, "Button was clicked!"));
         frame.getContentPane().add(button);
+
+        button.setPreferredSize(new Dimension(100, 50));
+
+        frame.setLayout(new FlowLayout());
 
         // Display the frame
         frame.setVisible(true);

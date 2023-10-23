fc510a005cd0a63f6302ab0c2ce925fd88c98ffe
Eric <huajimodi@gmail.com>
2023-10-23 13:59:22 -0400

    Updated the song class

--- src/entity/song.java
+++ src/entity/song.java
@@ -1,4 +1,21 @@
 package entity;
 
+import javax.xml.namespace.QName;
+
 public class song {
+    private String name;
+    private artist artist;
+
+    song(String name, artist artist){
+        this.name = name;
+        this.artist = artist;
+    }
+
+    public entity.artist getArtist() {
+        return artist;
+    }
+
+    public String getName() {
+        return name;
+    }
 }

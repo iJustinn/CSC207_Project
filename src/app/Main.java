262a6dc759dd1ecd7ce4c5595042a34a4e036f3a
Yihang Cai <Yihang.cai@mail.utoronto.ca>
2023-10-23 14:54:25 -0400

    basic structure created

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,23 @@
 package app;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        
+        SwingUtilities.invokeLater(() -> createAndShowGUI());
+    }
+
+    private static void createAndShowGUI() {
+        // Create a frame
+        JFrame frame = new JFrame("Swing Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+
+        // Create a button and add it to the frame
+        JButton button = new JButton("Click Me!");
+        button.addActionListener(e -> JOptionPane.showMessageDialog(frame, "Button was clicked!"));
+        frame.getContentPane().add(button);
+
+        // Display the frame
+        frame.setVisible(true);
     }
 }

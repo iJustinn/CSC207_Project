27c8bdf0c18ae67b4e5843e80e12e77b734ddbf4
Gordon <gordonchen2014@gmail.com>
2023-11-06 17:45:28 -0500

    docs: SpotifyDAO

--- src/data_access/SpotifyDataAccessObject.java
+++ src/data_access/SpotifyDataAccessObject.java
@@ -8,18 +8,22 @@ import use_case.search_album.SearchAlbumDataAccessInterface;
 import spotify.services.SearchAlbumService;
 import spotify.models.AlbumDto;
 
+/**
+ * A DataAccessObject used to get data from the Spotify API
+ */
 public class SpotifyDataAccessObject implements SearchAlbumDataAccessInterface {
 
-    private final SearchAlbumService searchAlbum = new SearchAlbumService();
+    private final SearchAlbumService searchAlbum;
     private final AlbumFactory albumFactory;
 
-    public SpotifyDataAccessObject(AlbumFactory albumFactory) {
+    public SpotifyDataAccessObject(AlbumFactory albumFactory,
+                                   SearchAlbumService searchAlbumService) {
         this.albumFactory = albumFactory;
+        this.searchAlbum = searchAlbumService;
     }
 
     @Override
     public Album[] searchAlbumsByString(String albumName) {
-        // Returns a list of AlbumDto
         AlbumDto[] albums = searchAlbum.requestSearchAlbum(albumName);
         return albumFactory.createMany(albums);
     }

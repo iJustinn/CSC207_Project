91190fd0a8efb6377f643c71d39d5e675474ef28
huan3264 <towaxun.huang@mail.utoronto.ca>
2023-11-10 18:34:55 -0500

    modfied playlist

--- src/entity/Playlist.java
+++ src/entity/Playlist.java
@@ -3,16 +3,17 @@
 package entity;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Playlist {
     private String name;
     private int numberOfSongs;
     private Date date;
-    private Map<Integer, Song> songs;
+    private HashMap<String, Song> songs;
 
     // Constructor
-    public Playlist(String name, int numberOfSongs, Date date, Map<Integer, Song> songs) {
+    public Playlist(String name, int numberOfSongs, Date date, HashMap<String, Song> songs) {
         this.name = name;
         this.numberOfSongs = numberOfSongs;
         this.date = date;
@@ -44,20 +45,15 @@ public class Playlist {
         this.date = date;
     }
 
-    public Map<Integer, Song> getSongs() {
+    public HashMap<String, Song> getSongs() {
         return songs;
     }
 
-    public void setSongs(Map<Integer, Song> songs) {
+    public void setSongs(HashMap<String, Song> songs) {
         this.songs = songs;
     }
 
     public void getSong() {
 
-    }
-
-    public void addSong(Song song) {
-        this.songs.put(this.songs.size() + 1, song);
-        this.numberOfSongs = this.songs.size();
     }
 }

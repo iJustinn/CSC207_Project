cfb672960d9f91361a8a8e2dff417cb9ed44bb44
iJustin <ijustinzhong@gmail.com>
2023-11-13 16:04:59 -0500

    added back button for sub-search interfaces

--- src/app/gui/GUI.java
+++ src/app/gui/GUI.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GUI {
+    private static JButton backButton;
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Music Management System");
@@ -17,7 +19,6 @@ public class GUI {
             // Panels for different functionalities
             JPanel searchPanel = createSearchPanel(cardLayout, cardPanel);
             JPanel playlistPanel = createPlaylistPanel();
-//            JPanel songPanel = createSongPanel();
             JPanel songSearchPanel = createSongSearchPanel();
             JPanel albumSearchPanel = createAlbumSearchPanel();
             JPanel artistSearchPanel = createArtistSearchPanel();
@@ -25,25 +26,20 @@ public class GUI {
             // Adding panels to the card layout
             cardPanel.add(searchPanel, "SearchPanel");
             cardPanel.add(playlistPanel, "PlaylistPanel");
-//            cardPanel.add(songPanel, "SongPanel");
             cardPanel.add(songSearchPanel, "SongSearchPanel");
             cardPanel.add(albumSearchPanel, "AlbumSearchPanel");
             cardPanel.add(artistSearchPanel, "ArtistSearchPanel");
 
-            // Button panel
+            // Button panel with Back button
             JPanel buttonPanel = new JPanel();
-            buttonPanel.add(new JButton("Search") {{
-                setForeground(Color.BLACK);
-                addActionListener(e -> cardLayout.show(cardPanel, "SearchPanel"));
-            }});
-            buttonPanel.add(new JButton("Playlists") {{
-                setForeground(Color.BLACK);
-                addActionListener(e -> cardLayout.show(cardPanel, "PlaylistPanel"));
-            }});
-//            buttonPanel.add(new JButton("Songs") {{
-//                setForeground(Color.BLACK);
-//                addActionListener(e -> cardLayout.show(cardPanel, "SongPanel"));
-//            }});
+//            buttonPanel.add(createNavButton("Back", "SearchPanel", cardLayout, cardPanel, buttonPanel));
+            buttonPanel.add(createNavButton("Playlists", "PlaylistPanel", cardLayout, cardPanel, buttonPanel));
+            buttonPanel.add(createNavButton("Search", "SearchPanel", cardLayout, cardPanel, buttonPanel));
+
+            // Create and add the Back button
+            backButton = createNavButton("Back", "SearchPanel", cardLayout, cardPanel, buttonPanel);
+//            backButton.setVisible(false); // Initially invisible
+            buttonPanel.add(backButton);
 
             // Adding panels to the frame
             frame.getContentPane().add(cardPanel, BorderLayout.CENTER);
@@ -58,26 +54,35 @@ public class GUI {
         });
     }
 
+    private static JButton createNavButton(String name, String panelName, CardLayout cardLayout, JPanel cardPanel, JPanel buttonPanel) {
+        JButton button = new JButton(name);
+        button.setForeground(Color.BLACK);
+        button.addActionListener(e -> {
+            cardLayout.show(cardPanel, panelName);
+//            updateBackButtonVisibility(buttonPanel, name);
+        });
+        return button;
+    }
+
+//    private static void updateBackButtonVisibility(JPanel buttonPanel, String buttonName) {
+//        // Show the Back button only when not on the main Search panel
+//        backButton.setVisible(!"Search".equals(buttonName));
+////        backButton.setVisible(!"Playlists".equals(buttonName));
+//    }
+
     private static JPanel createSearchPanel(CardLayout cardLayout, JPanel cardPanel) {
         JPanel panel = new JPanel(new BorderLayout());
 
         JPanel searchBoxPanel = new JPanel(new FlowLayout(FlowLayout.CENTER)); // adding sub-panel for search box
-
         JTextField searchText = new JTextField(); // text input box
         searchText.setColumns(21); // width
         searchText.setBackground(Color.LIGHT_GRAY); // background color
         searchText.setForeground(Color.BLACK); // text color
 
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        buttonPanel.add(new JButton("Song") {{
-            addActionListener(e -> cardLayout.show(cardPanel, "SongSearchPanel"));
-        }});
-        buttonPanel.add(new JButton("Album") {{
-            addActionListener(e -> cardLayout.show(cardPanel, "AlbumSearchPanel"));
-        }});
-        buttonPanel.add(new JButton("Artist") {{
-            addActionListener(e -> cardLayout.show(cardPanel, "ArtistSearchPanel"));
-        }});
+        buttonPanel.add(createNavButton("Song", "SongSearchPanel", cardLayout, cardPanel, buttonPanel));
+        buttonPanel.add(createNavButton("Album", "AlbumSearchPanel", cardLayout, cardPanel, buttonPanel));
+        buttonPanel.add(createNavButton("Artist", "ArtistSearchPanel", cardLayout, cardPanel, buttonPanel));
 
         searchBoxPanel.add(searchText); // adding search box to sub-panel
         panel.add(searchBoxPanel, BorderLayout.NORTH);
@@ -88,8 +93,7 @@ public class GUI {
     }
 
     private static JPanel createPlaylistPanel() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
+        JPanel panel = new JPanel(new BorderLayout());
 
         JLabel playlistLabel = new JLabel("Playlists Management Interface", SwingConstants.CENTER);
         playlistLabel.setFont(new Font("Arial", Font.PLAIN, 16)); // text font & size
@@ -98,18 +102,6 @@ public class GUI {
         panel.setBackground(Color.WHITE);
         return panel;
     }
-
-//    private static JPanel createSongPanel() {
-//        JPanel panel = new JPanel();
-//        panel.setLayout(new BorderLayout());
-//
-//        JLabel SonsLabel = new JLabel("Songs Management Interface", SwingConstants.CENTER);
-//        SonsLabel.setFont(new Font("Arial", Font.PLAIN, 16)); // text font & size
-//        panel.add(SonsLabel, BorderLayout.CENTER);
-//
-//        panel.setBackground(Color.WHITE);
-//        return panel;
-//    }
 
     private static JPanel createSongSearchPanel() {
         JPanel panel = new JPanel();

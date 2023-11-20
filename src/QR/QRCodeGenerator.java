646612780b7f4571a5df28e08f95a60a1d3383c2
Eric <huajimodi@gmail.com>
2023-11-20 19:59:27 -0400

    Testing new feature

---
+++ src/QR/QRCodeGenerator.java
@@ -0,0 +1,37 @@
+package QR;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+public class QRCodeGenerator {
+    public static void generateQRCodeImage(String text, int width, int height, String filePath)
+            throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        Map<EncodeHintType, String> hints = new HashMap<>();
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, hints);
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+    }
+
+    public static void main(String[] args) {
+        String githubURL = "https://github.com/[YourUsername]/[YourRepo]"; // Replace with your URL
+        String qrCodeFilePath = "path/to/QRCode.png"; // Replace with your desired file path
+
+        try {
+            generateQRCodeImage(githubURL, 350, 350, qrCodeFilePath);
+            System.out.println("QR Code generated at: " + qrCodeFilePath);
+        } catch (WriterException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

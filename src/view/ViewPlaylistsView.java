fd8761e474d221cc1fb32a3e6df8fc2612cfda51
Alessange <56106326+Alessange@users.noreply.github.com>
2023-11-28 21:49:57 -0500

    add functionality: whenever open, view_playlist automatically

--- src/view/ViewPlaylistsView.java
+++ src/view/ViewPlaylistsView.java
@@ -1,7 +1,6 @@
 package view;
 
 import app.GUI_View_Playlists;
-import interface_adapter.create_playlist.CreatePlaylistController;
 import interface_adapter.delete_playlist.DeletePlaylistController;
 import interface_adapter.view_playlists.ViewPlaylistsViewModel;
 import interface_adapter.view_playlists.ViewPlaylistsController;
@@ -43,6 +42,11 @@ public class ViewPlaylistsView extends JPanel implements PropertyChangeListener,
 
         viewModel.addPropertyChangeListener(this);
         initUI();
+        try {
+            playlistsController.execute("Alice");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void initUI() {
@@ -58,27 +62,45 @@ public class ViewPlaylistsView extends JPanel implements PropertyChangeListener,
         createPlaylistButton.addActionListener(this);
         deletePlaylistButton.addActionListener(this);
         refreshButton.addActionListener(this);
+        setupMouseListener();
+    }
+
+    private void setupMouseListener() {
+        playlistsList.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                int index = playlistsList.locationToIndex(evt.getPoint());
+                if (index >= 0) {
+                    String selectedPlaylist = playlistsList.getModel().getElementAt(index);
+                    try {
+                        viewSongController.execute("Alice", selectedPlaylist);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    GUI_View_Playlists.switchToSongView();
+                }
+            }
+        });
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == createPlaylistButton){
+        if (e.getSource() == createPlaylistButton) {
             GUI_View_Playlists.switchToCreateView();
-        }
-        if (e.getSource() == deletePlaylistButton) {
+        } else if (e.getSource() == deletePlaylistButton) {
             String selectedPlaylist = playlistsList.getSelectedValue();
             if (selectedPlaylist != null && !selectedPlaylist.isEmpty()) {
                 try {
                     deletePlaylistController.deletePlaylist(selectedPlaylist);
+                    playlistsController.execute("Alice");
                 } catch (IOException ex) {
-                    throw new RuntimeException(ex);
+                    ex.printStackTrace();
                 }
             } else {
                 JOptionPane.showMessageDialog(this, "Please select a playlist to delete.", "No Selection", JOptionPane.WARNING_MESSAGE);
             }
         } else if (e.getSource() == refreshButton) {
             try {
-                playlistsController.execute("Alice"); // Example username
+                playlistsController.execute("Alice");
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }

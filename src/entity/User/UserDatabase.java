b6142ed7b9d7a95c5ef7db2396d1834b77673b21
Alessange <56106326+Alessange@users.noreply.github.com>
2023-11-29 21:19:15 -0500

    add a method to get playlist in enetity of userdatabase

--- src/entity/User/UserDatabase.java
+++ src/entity/User/UserDatabase.java
@@ -39,18 +39,4 @@ public class UserDatabase {
         return Optional.ofNullable(playlists.get(playlistName));
     }
 
-    // Method to add a song to a specific playlist
-    public boolean addSongToPlaylist(String playlistName, Song newSong) {
-        Playlist playlist = playlists.get(playlistName);
-        if (playlist == null) {
-            // Playlist does not exist
-            return false;
-        }
-        int newSongId = playlist.getSongs().size() + 1;
-        playlist.getSongs().put(String.valueOf(newSongId), newSong);
-        playlist.setNumberOfSongs(playlist.getSongs().size());
-        return true;
-    }
-
-    // More methods could be added for additional functionality, like deleting a playlist, updating a playlist, etc.
 }

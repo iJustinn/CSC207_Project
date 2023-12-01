72708764ebf460a44e7fe50d2650d8c8d1680a97
Eric Wei <huajimodi@gmail.com>
2023-12-01 10:05:34 -0500

    Added Test Cases for Album entity

---
+++ test/entity/album/AlbumFullTest.java
@@ -0,0 +1,50 @@
+package entity.album;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlbumFullTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getArtists() {
+    }
+
+    @Test
+    void getReleaseDate() {
+    }
+
+    @Test
+    void getGenres() {
+    }
+
+    @Test
+    void getPopularity() {
+    }
+
+    @Test
+    void getLabel() {
+    }
+
+    @Test
+    void getTracklist() {
+    }
+}

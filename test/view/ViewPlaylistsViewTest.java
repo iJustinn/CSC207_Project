ef18724ee905dd2a6259f9ad2b8c5b09e84bf676
Eric <huajimodi@gmail.com>
2023-12-03 19:33:35 -0400

    Implmented tests for viewplaylist view

--- test/view/ViewPlaylistsViewTest.java
+++ test/view/ViewPlaylistsViewTest.java
@@ -1,5 +1,7 @@
 package view;
 
+import interface_adapter.create_playlist.CreatePlaylistController;
+import interface_adapter.create_playlist.CreateViewModel;
 import interface_adapter.delete_playlist.DeletePlaylistController;
 import interface_adapter.delete_playlist.DeletePlaylistViewModel;
 import interface_adapter.view_playlists.ViewPlaylistsController;
@@ -14,51 +16,77 @@ import javax.swing.*;
 
 import java.io.IOException;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
-public class ViewPlaylistsViewTest {
+class ViewPlaylistsViewTest {
 
     @Mock
-    private ViewPlaylistsViewModel mockViewModel;
+    private ViewPlaylistsViewModel viewModel;
     @Mock
-    private ViewPlaylistsController mockPlaylistsController;
+    private ViewPlaylistsController playlistsController;
     @Mock
-    private ViewSongController mockViewSongController;
+    private ViewSongController viewSongController;
     @Mock
-    private DeletePlaylistController mockDeletePlaylistController;
+    private DeletePlaylistController deletePlaylistController;
     @Mock
-    private DeletePlaylistViewModel mockDeletePlaylistViewModel;
+    private DeletePlaylistViewModel deletePlaylistViewModel;
+    @Mock
+    private CreateViewModel createViewModel;
+    @Mock
+    private CreatePlaylistController createPlaylistController;
 
+    private ViewPlaylistsView view;
 
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        view = new ViewPlaylistsView(viewModel, playlistsController, viewSongController, deletePlaylistController, deletePlaylistViewModel, createViewModel, createPlaylistController);
+    }
 
     @Test
-    void testPlaylistSelectionAndDelete() throws IOException {
-        // Assume there is a playlist called "TestPlaylist"
-        java.util.List<String> testPlaylists = new java.util.ArrayList<>();
-        testPlaylists.add("TestPlaylist");
+    void shouldDisplayPlaylistsOnPropertyChange() {
+        // Given
+        var playlists = java.util.List.of("Playlist 1", "Playlist 2");
+        when(viewModel.getState().getPlaylists());
 
-        // Simulate the view model updating the list of playlists
+        // When
+        viewModel.firePropertyChanged(); // Simulate property change
 
-        // Simulate a property change event
-
-        // Select a playlist in the view
-
-        // Simulate delete button click
-
-        // Verify that the delete playlist controller's method was called
-        verify(mockDeletePlaylistController, times(1)).deletePlaylist("TestPlaylist");
-
-        // You can also verify if the view updated correctly (e.g., showing a dialog)
-    }
-
-    // Utility method to access private fields for testing
-    private Object getField(Object obj, String fieldName) {
-        try {
-            java.lang.reflect.Field field = obj.getClass().getDeclaredField(fieldName);
-            field.setAccessible(true);
-            return field.get(obj);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        // Then
+        var listModel = (DefaultListModel<String>) view.playlistsList.getModel();
+        assertEquals(playlists.size(), listModel.getSize());
+        for (int i = 0; i < playlists.size(); i++) {
+            assertEquals(playlists.get(i), listModel.getElementAt(i));
         }
     }
+
+    @Test
+    void shouldInvokeDeletePlaylistWhenDeleteButtonPressed() throws IOException {
+        // Given
+        String selectedPlaylist = "Playlist 1";
+        view.playlistsList.setSelectedValue(selectedPlaylist, true);
+
+        // When
+        view.deletePlaylistButton.doClick();
+
+        // Then
+        verify(deletePlaylistController).deletePlaylist(selectedPlaylist);
+    }
+
+    @Test
+    void shouldShowErrorMessageWhenDeletePlaylistFails() throws IOException {
+        // Given
+        String selectedPlaylist = "Playlist 1";
+        view.playlistsList.setSelectedValue(selectedPlaylist, true);
+        doThrow(new IOException()).when(deletePlaylistController).deletePlaylist(anyString());
+
+        // When
+        view.deletePlaylistButton.doClick();
+
+        // Then
+        // You can check if an error dialog was displayed, but this requires additional setup with JOptionPane mocking.
+    }
+
+    // Additional tests for createPlaylistButton and refreshButton can be created in a similar manner.
 }

c96ae0a6462f47cb8ce1e5e2c283b9717eacf7aa
Alessange <56106326+Alessange@users.noreply.github.com>
2023-12-03 18:15:01 -0500

    Done testing for getsongs interface_adapater

---
+++ test/interface_adapter/search_album/SearchAlbumControllerTest.java
@@ -0,0 +1,12 @@
+package interface_adapter.search_album;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchAlbumControllerTest {
+
+    @Test
+    void execute() {
+    }
+}

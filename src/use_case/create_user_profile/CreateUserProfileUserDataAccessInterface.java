1575e27fe4c056e4be39933571e2f08595e729cd
iJustin <ijustinzhong@gmail.com>
2023-12-03 16:00:50 -0500

    deleting unused code/files

--- src/use_case/create_user_profile/CreateUserProfileUserDataAccessInterface.java
+++
@@ -1,6 +0,0 @@
-package use_case.create_user_profile;
-
-public class CreateUserProfileUserDataAccessInterface {
-    public void createUserProfile(String currentUserId, String profileInformation) {
-    }
-}
